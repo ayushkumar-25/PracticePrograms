60ba650e54fe5e0f8089a2a0d2726312205eb836
ayushkumar25 <ayush2608@gmail.com>
2020-10-12 21:32:18 +0530

    class creation

---
+++ OOP concepts/demo.py
@@ -0,0 +1,10 @@
+class Computer:
+    def config():
+        print("i5, 16GB, 1TB")
+
+a = 9
+com1 = Computer()
+print(type(a))
+print(type(com1))
+
+'''a is object of int(in-built) and com1 is object of Computer(user defined)'''
