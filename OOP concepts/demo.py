f2da325180b098cf63c4ba07ecdc631fb3e5b624
ayushkumar25 <ayush2608@gmail.com>
2020-10-13 12:08:47 +0530

    __init__

--- OOP concepts/demo.py
+++ OOP concepts/demo.py
@@ -1,12 +1,19 @@
 class Computer:
+    
+    def __init__(self):
+        print("init")
+    
     def config(self):
         print("i5, 16GB, 1TB")
 
+
 com1 = Computer()
 com2 = Computer()
+com3 = Computer()
 
-Computer.config(com1)
-Computer.config(com2)
 
 com1.config()
-com1.config()
+com2.config()
+
+# __init__ will be automatically called for each object created.
+# Here 3 objects is created (com1, com2, com3) therefore __init__ will be called for 3 times.
